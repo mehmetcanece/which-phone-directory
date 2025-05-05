1cd2dc6beafb4d9f290769a4237ba2675a08cd5b
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-05-05 20:49:43 +0300

    entegrasyon basladi, handlesubmit fonksiyonu degisti ve main.py guncellendi

--- backend/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routers import home, recognition, filter_criteria
-
-app = FastAPI(title="Which Phone Backend")
-
-app.include_router(home.router)
-app.include_router(recognition.router)
-app.include_router(filter_criteria.router)
