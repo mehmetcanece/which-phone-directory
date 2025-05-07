184f53db6e66dd912b8284ebebb0fb8349e204c4
Aykan Bican <eciraykan.bican@gmail.com>
2025-05-07 22:51:37 +0300

    first run of training the model, added epoch logs

---
+++ computer_vision/code/train_model.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    '../train_images',
+    validation_split=0.2,
+    subset="training",
+    seed=42,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    '../train_images',
+    validation_split=0.2,
+    subset="validation",
+    seed=42,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+class_names = train_dataset.class_names
+print("Class Names:", class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+
+base_model = MobileNetV2(input_shape=(224, 224, 3),
+                         include_top=False,
+                         weights='imagenet')
+base_model.trainable = False 
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(len(class_names), activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_dataset,
+          validation_data=validation_dataset,
+          epochs=10)
+
+model.save("phone_brand_classifier.h5")
