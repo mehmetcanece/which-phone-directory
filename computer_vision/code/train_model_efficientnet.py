6f810bf6f5249890fba3d2d7b9ec25349eef57ab
Aykan Bican <eciraykan.bican@gmail.com>
2025-05-09 17:31:13 +0300

    added efficientnet code and epoch log

---
+++ computer_vision/code/train_model_efficientnet.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, regularizers
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.callbacks import EarlyStopping
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    '../train_images',
+    validation_split=0.2,
+    subset="training",
+    seed=42,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    '../train_images',
+    validation_split=0.2,
+    subset="validation",
+    seed=42,
+    image_size=(224, 224),
+    batch_size=32
+)
+
+class_names = train_dataset.class_names
+print("Class Names:", class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+
+base_model = EfficientNetB0(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights='imagenet')
+base_model.trainable = True
+
+model = models.Sequential([
+    base_model,
+    layers.GlobalAveragePooling2D(),
+    layers.Dropout(0.3),
+    layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(len(class_names), activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+early_stop = EarlyStopping(monitor='val_loss', patience=4, restore_best_weights=True)
+
+model.fit(
+    train_dataset,
+    validation_data=validation_dataset,
+    epochs=20,
+    callbacks=[early_stop]
+)
+
+model.save("phone_brand_classifier_efficientnet.h5")
+
